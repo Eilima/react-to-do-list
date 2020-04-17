ac17766696c000185cf15229ec891f4ccb7c116a
ExciteTaco <ericklima34@gmail.com>
2020-04-16 23:26:37 -0500

    Add some comments to the parent component TodoList

--- react-to-do-list/src/Components/TodoList.js
+++ react-to-do-list/src/Components/TodoList.js
@@ -5,19 +5,26 @@ import TaskLeft from "./TaskLeft";
 import { FilterButtons } from "./FilterButtons";
 
 export class TodoList extends React.Component {
+  //Setting the state for parent component
   state = {
+    //A array with nested arrays for the values of the each individual 'task'
     tasks: [],
     tasksToShow: "all",
   };
 
+  //Method that adds the value passed to it to the states array, "tasks".
   addTask = (value) => {
+    //Calls setState to update state with arguement passed to it and its current contents
     this.setState({ tasks: [value, ...this.state.tasks] });
   };
-
+  //Used to update the value of the state of a "task"
+  //Accepts the id 'task' that will be updated.
   toggleComplete = (id) => {
-    console.log("test");
     this.setState({
+      //Iterates through each value of the "tasks" array
       tasks: this.state.tasks.map((task) => {
+        //If the current iteration of the elements id is equal to the one that was passed
+        //will not change any of the other values in the array only will set 'complete' value to its opposite value
         if (task.id === id) {
           return {
             ...task,
@@ -29,7 +36,8 @@ export class TodoList extends React.Component {
       }),
     });
   };
-
+  //Method that handle updating state with the string passed to it.
+  //Used for filter buttons
   handleClick = (string) => {
     this.setState({
       tasksToShow: string,
