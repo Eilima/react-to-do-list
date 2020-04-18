90b2eef3670fba0c01ed35640f83fbbc0c0ef813
ExciteTaco <ericklima34@gmail.com>
2020-04-18 11:59:44 -0500

    Implement material ui AppBar component for header

--- react-to-do-list/src/Components/Header.js
+++ react-to-do-list/src/Components/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
 
 export class Header extends React.Component {
   render() {
     return (
-      <header style={{ color: "#3f51b5" }}>
-        <h1>
-          <center>REACT TODO LIST APP!</center>
-        </h1>
-      </header>
+      <div>
+        <AppBar position="static" color="primary">
+          <Typography variant="h3">React App Todo</Typography>
+        </AppBar>
+      </div>
     );
   }
 }
